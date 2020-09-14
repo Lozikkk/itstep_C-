423ae0e7c8184f97c8c2adab5b3c93d6532f1863
Lozikkk <lozikanet@gmail.com>
2020-09-14 16:32:18 +0300

    AddMenu

--- Git_Culc.cpp
+++ Git_Culc.cpp
@@ -41,13 +41,28 @@ void Menu_Calc()
 	{
 		float Res1, Res2, Res3, Res4;
 		float a, b;
+		cout << "\t\t\t.......:::Calculator:::.......\n\n";
+		cout << "\t\t\t.........7    8    9..........\n\n";
+		cout << "\t\t\t.........4    5    6..........\n\n";
+		cout << "\t\t\t.........1    2    3..........\n\n";
+		cout << "\t\t\t.........     0     ..........\n\n";		
 		cout << "\tEnter the first number\t";
 		cin >> a;
+		cout << "\t\t\t -----------------------------\n";
+		cout << "\t\t\t|   1  |   2  |   3   |   4   |\n";
+		cout << "\t\t\t|-----------------------------|\n";
+		cout << "\t\t\t|   +  |   -  |   *   |   /   |\n";
+		cout << "\t\t\t|-----------------------------|\n\n";
 		cout << "\tPlease select your action!! 5 Exit.  ";
 		cin >> Diya;
 		if (Diya != 5)
 		{
-			cout << "\tEnter the second number  ";
+			cout << "\t\t\t.......:::Calculator:::.......\n\n";
+			cout << "\t\t\t.........7    8    9..........\n\n";
+			cout << "\t\t\t.........4    5    6..........\n\n";
+			cout << "\t\t\t.........1    2    3..........\n\n";
+			cout << "\t\t\t.........     0     ..........\n\n";
+			cout << "\tEnter the second number  ";		
 			cin >> b;
 			if (Diya == 1)
 			{
